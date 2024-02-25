1edda18d76b0d7dd5c5753cd28e473270e4318e8
Karan <karankpatil2016@gmail.com>
2024-02-26 00:01:59 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppointmentListComponent } from "./appointment-list/appointment-list.component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AppointmentListComponent, FormsModule]
 })
 export class AppComponent {
-  title = 'Appointment';
+  title = 'todo-list';
+  log(text : string) : void{
+    var message = 'Message' + text;
+    console.log(message);
+    
+    
+  }
+  
 }
